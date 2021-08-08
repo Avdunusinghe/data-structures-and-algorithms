f324ffb1d5bf92dd12a420da4ff7333dc20adf19
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-08-08 12:36:01 +0530

    link class created

---
+++ src/structures/linkedlist/theory/Link.java
@@ -0,0 +1,22 @@
+package structures.linkedlist.theory;
+
+public class Link {
+	
+	public int iData;
+	public Link next;
+	
+	public Link(int iData, Link next) {
+		
+		this.iData = iData;
+		this.next = next;
+		
+	}
+	
+	public void disPlayLink() {
+		
+		System.out.println("iData");
+		
+	}
+	
+
+}
