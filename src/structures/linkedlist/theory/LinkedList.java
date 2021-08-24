8ce2a74e984116a3e90b80759bc1dce541028ae2
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-08-24 17:34:22 +0530

    deletemiddle

--- src/structures/linkedlist/theory/LinkedList.java
+++ src/structures/linkedlist/theory/LinkedList.java
@@ -7,11 +7,13 @@ public class LinkedList {
 	public LinkedList(Link first) {
 		
 		this.first = null;
+		
 	}
 	
 	public boolean isEmpty() {
 		
 		return(first == null);
+		
 	}
 	
 	public boolean find(int id) {
@@ -27,20 +29,25 @@ public class LinkedList {
 			else {
 				
 				current = current.next;
+				
 			}
 		}
 		
 		return false;
 	}
 	
+	//insert value to first link
 	public void insertFirst(int id) {
 		
 		Link newLink = new Link(id);
+		
 		newLink.next = first;
+		
 		first = newLink;
 		
 	}
 	
+	//insert value to Middle link
 	public void insertMiddle(int key, int id) {
 		
 		Link newLink = new Link(id);
@@ -52,6 +59,7 @@ public class LinkedList {
 			if(current.iData == key) {
 				
 				newLink.next = current.next;
+				
 				current.next = newLink;
 			}
 			else {
@@ -66,9 +74,38 @@ public class LinkedList {
 	public Link deleteFirst() {
 		
 		Link temp = first;
+		
 		first = first.next;
+		
 		return temp;
-		//test querygdfgdf
+		
 	}
-
+	
+	public Link deleteMiddle(int id) {
+		
+		Link previous = first;
+		
+		Link current = first;
+		
+		while(current != null) {
+			
+			if(current.iData == id) {
+				
+				Link temp = current;
+				
+				previous.next = current.next;
+				
+				return temp;
+			}
+			else {
+				
+				previous = current;
+				
+				current = current.next;
+			}
+			
+		}
+		return null;
+		
+	}
 }
