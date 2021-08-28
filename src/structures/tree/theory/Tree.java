7abff89d2380f5f1625a5e365f5a5c0b125dbd5b
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-08-28 14:55:03 +0530

    tree class update and node class insert done

--- src/structures/tree/theory/Tree.java
+++ src/structures/tree/theory/Tree.java
@@ -1,5 +1,56 @@
 package structures.tree.theory;
 
 public class Tree {
-
+	
+	private Node root;
+	
+	public Tree() {
+		
+		this.root = null;
+	}
+	
+	public void inset(int id, double value) {
+		
+		Node newNode = new Node();
+		newNode.iData = id;
+		newNode.dData = value;
+		
+		if(root == null) {
+			
+			root = newNode;
+		
+		}
+		else {
+			
+			Node current = root;
+			Node parent;
+			while(true) {
+				
+				parent = current;
+				
+				if(id < current.iData) {
+					current = current.leftNode;
+					
+					if(current == null) {
+						
+						current = current.leftNode = newNode;
+						return;
+					}
+					
+				}
+				else {
+					
+					current = current.leftNode;
+					if(current == null) {
+						
+						parent.rigthNode = newNode;
+						return;
+					}
+				}
+			}
+		}
+		
+		
+	}
+	
 }
